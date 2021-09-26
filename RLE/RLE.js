c7b501632e210f14c9abe999e34116dd1d288df4
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2021-09-26 16:13:52 +0500

    RLE

--- RLE/RLE.js
+++ RLE/RLE.js
@@ -2,6 +2,7 @@ const { strictEqual } = require('assert');
 let fs = require('fs');
 let i=0, n=1;
 let outcode="";
+let ost=0;
 fs.readFile("input.txt", "utf-8", function(err, data){ //читает данные в файле инпут, обращенные в формат utf-8
     if (err) throw err; //"выкидывает" при ошибке
     inText=data.toString(); //переводит данные в файле в переменную
@@ -11,21 +12,36 @@ fs.readFile("input.txt", "utf-8", function(err, data){ //читает данны
         while(symb == inText.charAt(i+n)){         //пока соседние знаки равны, n увеличивается
             n++;     
         }
-        if(n>=4) {                                      //если n превзошёл 4, то его надо сжать
-            if(n>=258){
-                m=Math.floor(n/258);      //округление до меньшего                           
-                outcode+=("#" + String.fromCharCode(255) + symb).repeat(m)+ "#" + String.fromCharCode(n-m*258-3) + symb; //если счетчик превысил 258, то после решетки идёт зашифрованное 255 и действите повторяется столько раз, чему равен m + идёт зашифрованное число меньше 255, если оно есть
-
+        if (inText.charAt(i)=='#'){
+            if (n>255){
+                m=Math.floor(n/255);
+                outcode+=('#'+ String.fromCharCode(255)+symb).repeat(m)
+                outcode+=('#'+ String.fromCharCode(n-(255*m))+symb);
             }
-            else
-            outcode+="#" + String.fromCharCode(n-3) + symb; //если же не превысил 255, то после решетки идёт зашифрованное число меньше 255, а потом сам символ
+            if (n<=255){
+                outcode+='#'+ String.fromCharCode(n)+symb
+            }
+            i=i+n;
+            n=1;
+            }
+        else{
+            if(n>=4) {                                      //если n превзошёл 4, то его надо сжать
+                if(n>259){
+                    m=Math.floor(n/259);//округление до меньшего
+                    ost=n-259*m;                                 
+                    outcode+=("#" + String.fromCharCode(255) + symb).repeat(m)
+                    if (ost>0)
+                        outcode+="#" + String.fromCharCode(ost) + symb;       
+                }
+                if (n<259)
+                    outcode+="#" + String.fromCharCode(n-4) + symb;  
+            }
+            else 
+                outcode+=symb.repeat(n);
+            i=i+n;
+            n=1;
         }
-        else 
-            outcode+=symb.repeat(n);       //записывает символы, число повторения которых меньше 4
-        i += n;             //запускает всё по новой
-        n = 1;
-        }
+    }     
    // console.log (outcode); 
-    fs.writeFileSync("outcode.txt", String(outcode));           //записывает всё в файл ауткод
-}
-)
+fs.writeFileSync("outcode.txt", String(outcode));           //записывает всё в файл ауткод
+}) 
--- RLE/RLE2.js
+++ RLE/RLE2.js
@@ -5,21 +5,19 @@ let outcode="";
 fs.readFile("outcode.txt", "utf-8", function(err, data){ //читает данные в файле ауткод, обращенные в формат utf-8
     if (err) throw err; //"выкидывает" при ошибке
     inText=data.toString(); //переводит данные в файле в переменную
-    while (i < inText.length){  //прогоняет i через весь текст
-        symb=inText.charAt(i); //переменная, в которую записывает прогоняемый знак        
-        if (symb=="#"){
-            nextsymb=inText.charAt(i+1);  //НЕ ЗАБЫВАТЬ ЧИТАТЬ КАЖДУЮ КОМАНДУ!!! НЕ ЗАБЫВАТЬ, ЧТО НАДО БРАТЬ ИЗ ФАЙЛА!!!(для себя)
-            numbersymb=nextsymb.charCodeAt();  //декодированное число, идущее после решетки           
-            outcode+=inText.charAt(i+2).repeat(numbersymb+3); //берем символ, который находится в 2 знаках от решетки, и повторяем его столько раз, чему равен декодированный numbersymb
-            i=i+3; //после этого, перенести i на 3 знака вперед, проверять следующие знаки в строке
-        }
-        else{
-            outcode+=symb; //если решетка не встречается, то продолжаем перебор
-            i++;
-        }
-}
-//console.log(outcode)
+    while (i<inText.length){
+    if ((inText.charAt(i)=='#')) {
+		if (inText.charAt(i+2)=='#')
+			outcode+=('#').repeat(inText.charCodeAt(i+1))
+		else 
+			outcode+=(inText.charAt(i+2)).repeat(inText.charCodeAt(i+1)+4)
+		i=i+3
+	}
+	else {
+        outcode+=inText.charAt(i);	
+        i++;
+	}
+	}
 fs.writeFileSync("decode.txt", String(outcode));
-}
-)
+})
 //String.charCodeAt
