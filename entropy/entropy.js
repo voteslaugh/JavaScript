3ff133e7e10098ece6c1c4a1b272d0611ae1b4d0
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2021-09-29 18:26:18 +0500

    Update entropy.js

--- entropy/entropy.js
+++ entropy/entropy.js
@@ -1,24 +1,27 @@
-let fs = require('fs');
-let arg = process.argv;
-let inputData; 
-let i, sum, entr = 0;
-let alph = new Array()
+let fs = require('fs');//объект для работы с файловой системой
+let arg = process.argv;//объект для работы с аргументами командной строки
+let entr = 0;
+let alph = new Array();//объект типа как-бы "массив"
+inputData = fs.readFileSync('test1.txt');//байтовый буфер //можно заменить на test2.txt/test3.txt
+inputData = inputData.toString();//преобразовывает байтовый буфер в строку
+for (i=0 ; i<inputData.length; i++){ //"прогоняем" i через строку, инициализируем алфавит
+	alph[inputData.charAt(i)] = 0;
+}
 
-inputData = fs.readFileSync('input.txt').toString();  //test1/test2/test3 на выбор
-
-
-for (i = 0 ; i<inputData.length; i++)   //инициализация алфавита
-	alph[inputData.charAt(i)] = 0
-
-for (i = 0 ; i<inputData.length; i++){      //записываем число повторов, объектов
+for (i=0 ; i<inputData.length; i++){		//отсчитываем число повторений букв, число ячеек
 	alph[inputData.charAt(i)]++;
 }
-sum=i;
-console.log(alph);
 
-if (sum>1){
+
+alphmemb = i;
+
+
+if (alphmemb>1){
 	for (i in alph)
 		entr-=(alph[i]/inputData.length)*(Math.log(alph[i]/inputData.length))     //энтропия
-	entr/=Math.log(sum)
+	entr/=Math.log(alphmemb);
 }
+
+console.log(alph);
+
 console.log(entr)
