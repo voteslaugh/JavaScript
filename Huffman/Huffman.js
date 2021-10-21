57950f4aa12b67d641f2369531964e1e1456a5ca
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2021-10-21 22:04:15 +0500

    Update Huffman.js

--- Huffman/Huffman.js
+++ Huffman/Huffman.js
@@ -1,75 +1,92 @@
 let fs = require('fs');
-function Node(letter, freq, used, father, code){
-    this.letter = letter;
-    this.freq = freq;
-    this.used = used;
-    this.father = father;
-    this.code = code;
-}
- 
-//данные из файла, перенесенные в строчные
 let inputData = (fs.readFileSync('input.txt')).toString();
+
 let alph = new Array();
- 
-//создание алфавита с подсчетом повторений
-for (let i = 0; i < inputData.length; i++){
+let tree = new Array();
+let i, num1, num2 = 0;
+let str = '';
+console.log(Node);
+
+function Node (_letter, freq, used, father, code){
+    this.letter=_letter;
+    this.freq=freq;
+    this.used=used;
+    this.father=father;
+    this.code=code;
+}
+
+for (let i = 0; i < inputData.length; i++){ //открывается массив, записываются элементы
     if (alph[inputData.charAt(i)])
         alph[inputData.charAt(i)]++;
     else{
         alph[inputData.charAt(i)] = 1;
     }
 }
-//создание массива с узлами
-let tree = new Array();
-for (let i in alph)
-{
-    tree.push(new Node(i, alph[i]));
+
+for (i in alph){            //для каждого символа заводится "ветка"
+    let n = new Node(i, alph[i], false, null, '');
+    tree.push(n);
 }
- 
-for (let i = 0; i < tree.length - 1; i++){
-    //Поиск узлов с минимальным кол-вом включений
-    let min1 = inputData.length;
-    let min2 = inputData.length;
-    let id1 = -1;
-    let id2 = -1;
-    for (let j = 0; j < tree.length; j++){
-        if (tree[j].freq < min1 && !tree[j].used){
-            min1 = tree[j].freq;
-            id1 = j;
-        }    
+
+while (tree[tree.length-1].freq != inputData.length){
+    min1=inputData.length;
+    min2=inputData.length;
+    for (i in tree){
+        if (tree[i].freq<=min1 && tree[i].used==0){
+            min1=tree[i].freq;
+            num1=i;
+        }
     }
- 
-    for (let j = 0; j < tree.length; j++){
-        if (tree[j].freq < min2 && j != id1 && !tree[j].used){
-            min2 = tree[j].freq;
-            id2 = j;
-        }    
+    tree[num1].used=true;
+    for (i in tree){
+        if (tree[i].freq<=min2 && tree[i].used==0){
+            min2=tree[i].freq;
+            num2=i;
+        }
     }
- 
-    if (id1 == -1 || id2 == -1){
-        break;
+    tree[num2].used=true;
+
+    n = new Node(tree[num1].letter+tree[num2].letter, tree[num1].freq+tree[num2].freq, 0, null, '');
+    tree.push(n);
+    tree[num1].used=true;
+    tree[num2].used=true;
+    tree[num1].father=tree.length-1;
+    tree[num2].father=tree.length-1;
+    let s=(tree[num1].letter).length;
+    if (s>1){
+        tree[num1].code='1';
+        tree[num2].code='0';
     }
-    //создание нового узла
-    let newString = tree[id1].letter + tree[id2].letter;
-    let newFreq = tree[id1].freq + tree[id2].freq;
-    tree.push(new Node(newString, newFreq));
-    //редактирование значений узлов
-    tree[id2].used = true;
-    tree[id1].used = true;
-    tree[id1].father = tree.length - 1;
-    tree[id2].father = tree.length - 1;
+    else{
+        tree[num1].code='0';
+        tree[num2].code='1';
+    }
 }
-//получение кода
-tree[tree.length - 1].code = '0';
-tree[tree.length - 1].used = true;
-for (let i = tree.length - 2; i >= 0; i--){
-    if (tree[tree[i].father].used){
-        tree[i].code = tree[tree[i].father].code + '1';
-        tree[tree[i].father].used = false;
-    }    
-    else
-        tree[i].code = tree[tree[i].father].code + '0';
- 
+for (i=tree.length-2; i!=-1; i--)
+    tree[i].code=tree[tree[i].father].code+tree[i].code;
+
+for (i=0; i<=inputData.length; i++){
+    for (j=0; j<tree.length/2; j++){
+        if (inputData[i]==tree[j].letter)
+            str+=tree[j].code
+    }
 }
-//вывод дерева
-console.log(tree);
+
+console.log(tree)
+console.log(str)
+
+//раскодировка
+
+let finaltree='';
+let p='';
+
+for (i=0;i<=str.length; i++){  //перезапись дерева
+    p+=str[i];
+    for (j=0; j<tree.length/2; j++){
+        if (p==tree[j].code){
+            finaltree+=tree[j].letter
+            p='';
+        }
+    }
+}
+console.log(finaltree);
