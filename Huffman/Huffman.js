12b838ea3a24adac28baffa021ea5faf3cec2455
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2021-10-25 14:58:12 +0500

    Update Huffman.js

--- Huffman/Huffman.js
+++ Huffman/Huffman.js
@@ -1,92 +1,72 @@
-let fs = require('fs');
-let inputData = (fs.readFileSync('input.txt')).toString();
-
+let fs = require("fs");
 let alph = new Array();
+let arg = process.argv;
+let inputData = fs.readFileSync(arg[2]).toString(); //записывает данные из файла в инпут дата
+
+function Node(letter, freq, used, father, code) {
+    this.letter = letter;
+    this.freq = freq;
+    this.used = used;
+    this.father = father;
+    this.code = code;
+}
+//инициализация алфавита
+for (let i = 0; i < inputData.length; i++) {
+    alph[inputData.charAt(i)] = 0;
+}
+for (let i = 0; i < inputData.length; i++) {
+    alph[inputData.charAt(i)] += 1;
+}
+
 let tree = new Array();
-let i, num1, num2 = 0;
-let str = '';
-console.log(Node);
 
-function Node (_letter, freq, used, father, code){
-    this.letter=_letter;
-    this.freq=freq;
-    this.used=used;
-    this.father=father;
-    this.code=code;
-}
-
-for (let i = 0; i < inputData.length; i++){ //открывается массив, записываются элементы
-    if (alph[inputData.charAt(i)])
-        alph[inputData.charAt(i)]++;
-    else{
-        alph[inputData.charAt(i)] = 1;
-    }
-}
-
-for (i in alph){            //для каждого символа заводится "ветка"
-    let n = new Node(i, alph[i], false, null, '');
+for (c in alph) {
+    let n = new Node(c, alph[c], false, null, '');
     tree.push(n);
 }
-
-while (tree[tree.length-1].freq != inputData.length){
-    min1=inputData.length;
-    min2=inputData.length;
-    for (i in tree){
-        if (tree[i].freq<=min1 && tree[i].used==0){
-            min1=tree[i].freq;
-            num1=i;
+trlng = tree.length;
+for (let i = 0; i < trlng - 1; i++) {
+    let min = inputData.length;
+    let first;
+    let second;
+    for (let k = 0; k < tree.length; k++) {
+        if (min > tree[k].freq && !tree[k].used) {
+            min = tree[k].freq;
+            first = k;
         }
     }
-    tree[num1].used=true;
-    for (i in tree){
-        if (tree[i].freq<=min2 && tree[i].used==0){
-            min2=tree[i].freq;
-            num2=i;
+    tree[first].used = true;
+    tree[first].father = tree.length;
+    tree[first].code = '0';
+    let trmin = inputData.length;
+    for (let i = 0; i < tree.length; i++) {
+        if (trmin > tree[i].freq && !tree[i].used) {
+            trmin = tree[i].freq;
+            second = i;
         }
     }
-    tree[num2].used=true;
 
-    n = new Node(tree[num1].letter+tree[num2].letter, tree[num1].freq+tree[num2].freq, 0, null, '');
+    tree[second].used = true;
+    tree[second].father = tree.length;
+    tree[second].code = '1';
+    let n = new Node(tree[first].letter + tree[second].letter, tree[first].freq + tree[second].freq, false, null, '')
     tree.push(n);
-    tree[num1].used=true;
-    tree[num2].used=true;
-    tree[num1].father=tree.length-1;
-    tree[num2].father=tree.length-1;
-    let s=(tree[num1].letter).length;
-    if (s>1){
-        tree[num1].code='1';
-        tree[num2].code='0';
-    }
-    else{
-        tree[num1].code='0';
-        tree[num2].code='1';
+}
+let final = [];
+for (let i = 0; i < trlng; i++) {
+    let g = i;
+    final[tree[g].letter] = '';
+    while (tree[g].father != null) {
+        final[tree[i].letter] = tree[g].code + final[tree[i].letter];
+        g = tree[g].father;
+
     }
 }
-for (i=tree.length-2; i!=-1; i--)
-    tree[i].code=tree[tree[i].father].code+tree[i].code;
+let str = ''; 
+for (let i = 0; i < trlng; i++) {
+    let j = inputData[i];
+    str += final[j];
 
-for (i=0; i<=inputData.length; i++){
-    for (j=0; j<tree.length/2; j++){
-        if (inputData[i]==tree[j].letter)
-            str+=tree[j].code
-    }
+
 }
-
-console.log(tree)
-console.log(str)
-
-//раскодировка
-
-let finaltree='';
-let p='';
-
-for (i=0;i<=str.length; i++){  //перезапись дерева
-    p+=str[i];
-    for (j=0; j<tree.length/2; j++){
-        if (p==tree[j].code){
-            finaltree+=tree[j].letter
-            p='';
-        }
-    }
-}
-console.log(finaltree);
+fs.writeFileSync(arg[3],str); //Записывает данные в файл
