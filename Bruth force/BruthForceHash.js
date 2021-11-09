d409e01eaf23f2796a6069099265b3ba8b312bf5
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2021-11-10 00:11:05 +0500

    bruth

--- Bruth force/BruthForceHash.js
+++ Bruth force/BruthForceHash.js
@@ -10,8 +10,16 @@ for (let i = 0; i < m; i++){
     valT+=t.charCodeAt(i);
 }
 
+let valS = 0;
+for (let i = 0; i < m; i++){
+    valS+=s.charCodeAt(i);
+}
 let count;
 for (let i=1; i<n-m+2; i++){
+    if (i>1){
+        valS = valS - s.charCodeAt(i-2) + s.charCodeAt(i+m-2);
+    }
+    if (valS===valT){
         count = 0;
         for (let j=i-1; j<i+m+1; j++){
             if (s[j]!==t[count]){
@@ -22,6 +30,7 @@ for (let i=1; i<n-m+2; i++){
             }
             count++;
         }
+    }
 
 }
 console.log(arr.join(', '));
