a9990b06b3ecc67a2c4b6705f27451c77b210061
Владислав Зубков КНМО-101 <usualvlad@gmail.com>
2021-11-29 21:59:15 +0500

    abs

--- Huffman/Huffman.js
+++ Huffman/Huffman.js
@@ -1,79 +1,129 @@
-let string='abrakadabra';
+let arg = process.argv;
 let tree = [];
-let alph = [];
-let num1, num2 = 0;
-let str = '';
-function Node (letter, freq, used, father, code){
-    this.letter=letter;
-    this.freq=freq;
-    this.used=used;
-    this.father=father;
-    this.code=code;
-}
-console.log(Node);
+let alphabet = [];
+let symbCodes = {};
 
-for (let i=0; i<string.length;i++){
-    alph[string.charAt(i)]=0;
+//Функция создания узла дерева
+function Node(letter, frequency, used, parent, code){
+    this.letter = letter;
+    this.frequency = frequency;
+    this.used = used;
+    this.parent = parent;
+    this.code = code;
 }
+//Считывание строки с консоли
+let string = arg[2].toString();
 
-for (let i=0; i<string.length;i++){
-    alph[string.charAt(i)]++;
-}
+console.log('Строка: ', string);
 
-for (let i in alph){
-    let n = new Node(i, alph[i], false, null, '');
-    tree.push(n);
+//Создание алфавита символов строки
+for (let i = 0 ; i < string.length; i++){
+    alphabet[string.charAt(i)] = 0;
 }
 
-while (tree[tree.length-1].freq !== string.length){
-    let min1=string.length;
-    let min2=string.length;
-    for (let i in tree){
-        if (tree[i].freq<=min1 && tree[i].used==0){
-            min1=tree[i].freq;
-            num1=i;
-        }
-    }
-    tree[num1].used=true;
-    for (let i in tree){
-        if (tree[i].freq<=min2 && tree[i].used===0){
-            min2=tree[i].freq;
-            num2=i;
-        }
-    }
-    tree[num2].used=true;
-
-    let n = new Node(tree[num1].letter+tree[num2].letter, tree[num1].freq+tree[num2].freq, 0, null, '');
-    tree.push(n);
-    tree[num1].father=tree.length-1;
-    tree[num2].father=tree.length-1;
-    let s=(tree[num1].letter).length;
-    tree[num1].code='1';
-    tree[num2].code='0';
-}
-for (let i=tree.length-2; i!==-1; i--) {
-    tree[i].code=tree[tree[i].father].code+tree[i].code;
-    if ((tree[i].letter).length===1)
-        alph[tree[i].letter]=tree[i].code;
+//Сопоставление символов алфавита с их частотой
+for (let i = 0 ; i < string.length; i++){
+    alphabet[string.charAt(i)]++;
 }
 
-for (let i=0; i<string.length; i++)
-    str+=alph[string[i]];
-
-console.log(tree)
-console.log(alph)
-console.log(str)
-
-let out='';
-let p='';
-
-for (let i=0;i<=str.length; i++){
-    p+=str[i];
-    for (let j in alph){
-        if (p===alph[j]){
-            out+=j
-            p='';
-        }
+//Обработка случая, когда в строке встречается только один символ
+let count = 0;
+for (let i in alphabet){
+    count++;
+}
+if (count === 1){
+    for (let i in alphabet){
+        alphabet[i] = '0';
+        symbCodes['0'] = i;
+        console.log('Коды символов:\n-', i, '0');
     }
 }
-console.log(out);
+else {
+    for (let i in alphabet){
+        let n = new Node(i, alphabet[i], 0, null, '');
+        tree.push(n);
+    }
+
+//Построение дерева
+    while (tree.some(elem => elem.used === false)){
+        let minIndex1, minIndex2;
+        freq = Infinity;
+        for (let i in tree){
+            if ((!tree[i].used) && (tree[i].frequency <= freq)){
+                minIndex1 = i;
+                freq = tree[i].frequency;
+            }
+        }
+        tree[minIndex1].used = 1;
+
+        freq = Infinity;
+        for (let i in tree){
+            if ((!tree[i].used) && (tree[i].frequency <= freq)){
+                minIndex2 = i;
+                freq = tree[i].frequency;
+            }
+        }
+
+        //Выход из цикла в случае, когда построение дерева дошло до корня
+        if (freq === Infinity){
+            break
+        }
+
+        tree[minIndex2].used = 1;
+
+        //Задание родительского элемента для найденных узлов
+        tree[minIndex1].parent = tree.length;
+        tree[minIndex2].parent = tree.length;
+
+        //Задание кода ветки от родительского эл-та к узлу
+        tree[minIndex1].code += '0';
+        tree[minIndex2].code += '1';
+
+        //Создание родительского элемента
+        let n = new Node(
+            tree[minIndex1].letter + tree[minIndex2].letter,
+            tree[minIndex1].frequency + tree[minIndex2].frequency,
+            0,
+            null,
+            '');
+        tree.push(n);
+    }
+
+    console.log('• Коды символов:')
+    for (let i in alphabet){
+        let codeOfSymb = '';
+        let j = 0;
+
+        while (i !== tree[j].letter){
+            j += 1;
+        }
+
+        do {
+            codeOfSymb = tree[j].code + codeOfSymb;
+            j = tree[j].parent;
+        } while ((tree[j].parent != null))
+
+        symbCodes[codeOfSymb] = i;
+        alphabet[i] = codeOfSymb;
+        console.log('-', i, codeOfSymb);
+    }
+}
+
+//Кодирование входной строки
+let codedString = string;
+for (let i in alphabet){
+    codedString = codedString.replace(RegExp(i, 'g'), alphabet[i]);
+}
+console.log('Закодированная строка: ', codedString);
+
+//Декодирование закодированной строки
+let decodedData = '';
+let symbol = ''
+for (let i in codedString){
+    symbol += codedString[i];
+    if (symbCodes[symbol] !== undefined){
+        decodedData += symbCodes[symbol];
+        symbol = '';
+    }
+}
+console.log('Раскодированная строка: ', decodedData)
